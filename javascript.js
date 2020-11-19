23415f4d5617280eb98a1409ce33107b48aa86dd
Malin Sjoblom <malin.sjoblom@hotmail.com>
2020-11-19 16:21:30 +0100

    cssDesign

--- javascript.js
+++ javascript.js
@@ -1,118 +1,74 @@
 //program should take income and expense inputs and show the result 
-
+// made an array to the three different 
 var listIncome = [];
 var listExpense = [];
-var listTotal = []; 
-
+var listTotal = [];
 
 var list;
 
 function add() {
-    
-    var operator = document.getElementById('operator').value;
-    var number = document.getElementById('value').value * 1;
-    var txt = document.getElementById('description').value;
-    var totalBudget = document.getElementById('totalBudget'); //value används när det finns input 
 
-   
+    var operator = document.getElementById("operator").value;
+    var number = document.getElementById("value").value * 1;
+    var txt = document.getElementById("description").value;
+    var totalBudget = document.getElementById("totalBudget"); //value används när det finns input 
+
+
 
     var div; // variabels list and div 
 
-    
 
-    if (operator === '+') {
+
+    if (operator === "+") {
         list = listIncome;
-        div = document.getElementById("income");
+        div = document.getElementById("income-h1");
 
         listTotal.push(Number(number)); //konverterar om det till number med Number
 
-    } // if its an income put in this field 
+    }
 
     else {
         list = listExpense;
-        div = document.getElementById("expense");
+        div = document.getElementById("expenses-h1");
 
         listTotal.push(Number(-number)); //konverterar om det till number med Number
 
     } // if not take it as an expense 
     //totalBudget(listIncome, listExpense)
 
-var total = 0;  //totalen måste vara nollställd pga den ska kunna räkna det vi skriver in i input 
+    var total = 0;  //totalen måste vara nollställd pga den ska kunna räkna det vi skriver in i input 
 
-for (var i = 0; i < listTotal.length; i++){
-    total += listTotal[i]; //total är 0 + 0 = det talet som finns i index
-
-
-}
-totalBudget.innerText = total //uträkningen kommer att synas på html under "totalbudget"
-
-
-
+    for (var i = 0; i < listTotal.length; i++) {
+        total += listTotal[i]; //total är 0 + 0 = det talet som finns i index
+    }
 
     console.log(listTotal)
+    totalBudget.innerText = total //uträkningen kommer att synas på html under "totalbudget"
 
-    list.push({ num: number, text: txt  });
+
+    list.push({ num: number, text: txt });
     div.innerHTML = "";
     for (var i = 0; i < list.length; i++) {
         div.innerHTML += list[i].text + " ";
-        div.innerHTML += list[i].num + " <br>";
+        div.innerHTML += list[i].num + "<br>";
     } //the list to see both income and expense 
     console.log(list.length)
 
 
-    
 
-    
 
-    function clear(){
+    function clear() {
 
         location.reload()
-    
+
        /*  console.log("hej")
-        list = [] */ } 
-        
-      var clearButton = document.querySelector("#clear")
-      console.log(clearButton)
-    
-      clearButton.addEventListener("click", clear);
-
-        /*document.getElementById('income').value=''}
-        console.log(clear) //the clear button that dosent work ? */
-    
-}
-
-
-
-   
-
-//function for calculate the total 
-function totalBudget( income, expense){
-    var totalBudget = income - expense 
-    console.log(totalBudget)
-}
-
- var value = totalBudget("0")
- var description = totalBudget("0")
-
- console.log(value + description)
-
-
-
-
-function calc() {
-    var a = parseInt(document.querySelector("#income1").value);
-    var b = parseInt(document.querySelector("#cost1").value);
-    var op = document.querySelector("#operator").value;
-    var calculate;
-    
-    if (op == "add") {
-    calculate = a + b;
-    } else if(op == "min") {
-    calculate = a - b;
-    
+        list = [] */ 
     }
-    document.querySelector("#result").innerHTML = calculate;
-    
-    console.log(calculate);
-    
-   }
+
+    var clearButton = document.querySelector("#clear")
+    console.log(clearButton)
+
+    clearButton.addEventListener("click", clear); 
+
+
+}
